5ab74bf7ea19e0ba7e43d200a04481e0eb140f40
kjuik <hiszpanskainkwizycja@gmail.com>
2013-09-25 18:37:45 +0200

    PlayerKiller
    Just as the last time: attach to CameraTarget, reference the Players and
    the camera. The BlockHeight should be -0.45.

---
+++ Assets/Scripts/CameraScripts/PlayerKiller.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class PlayerKiller : MonoBehaviour {
+
+	public List<Player> players;
+	public Camera cam;
+	public float blockHeight = -0.45f;
+	
+	void Update(){
+		
+			float cameraBottom = cam.ViewportToWorldPoint(new Vector3(0.5f,0f,0f)).y;
+		
+			foreach(Player p in players){
+				float playersBottom = p.transform.position.y - p.gameObject.transform.lossyScale.y * blockHeight;
+				
+				if (playersBottom < cameraBottom)
+					p.Squish();
+			}
+		
+	}
+	
+}
