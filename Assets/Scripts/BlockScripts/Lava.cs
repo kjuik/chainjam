8c806a7e62fc272adb19794b7358bba229f8900e
kjuik <hiszpanskainkwizycja@gmail.com>
2013-09-25 15:56:39 +0200

    Chain jam demo game
    init

---
+++ Assets/Scripts/BlockScripts/Lava.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lava : MonoBehaviour {
+
+	void OnCollisionEnter (Collision collision) {
+		
+		// If the player touches the lava, squish 'em!
+		if(collision.rigidbody.tag == "Player")
+		{
+			collision.rigidbody.GetComponent<Player>().Squish();
+		}
+    }
+}
