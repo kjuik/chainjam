b7ede80ee8bba3d2064fb85f7965a86d9ab0fcb7
JannickHP <jannick.h.petersen@gmail.com>
2013-09-26 15:59:40 +0200

    Lasers and size adjustments

--- Assets/Scripts/Game/BlockSpawner.cs
+++ Assets/Scripts/Game/BlockSpawner.cs
@@ -9,8 +9,8 @@ public class BlockSpawner : MonoBehaviour {
 	public int distance = 3;
 	public int maxSize = 4;
 	public int minSize = 1;
-	public int minX = -12;
-	public int maxX = 12;
+	public int minX = -25;
+	public int maxX = 25;
 	public int height = 1;
 	
 	// Use this for initialization
@@ -37,16 +37,21 @@ public class BlockSpawner : MonoBehaviour {
 	}
 	
 	void SpawnBlock(float y, float width){
-	
+	//Jannick tries to make two platforms the funky way
 		float x = Random.Range(minX, maxX);
+		float x2 = Random.Range(minX, maxX);
 		
 		Transform instance = (Transform) Instantiate(platform, new Vector3(x, y, 0), Quaternion.identity);
+		Transform instance2 = (Transform) Instantiate(platform, new Vector3(x2, y, 0), Quaternion.identity);
 		
 		Vector3 scale = instance.transform.localScale;
+		Vector3 scale2 = instance2.transform.localScale;
 		
 		scale.x = width;
+		scale2.x = width;
 		
 		instance.transform.localScale = scale;
+		instance2.transform.localScale = scale2;
 		
 	}
 	
