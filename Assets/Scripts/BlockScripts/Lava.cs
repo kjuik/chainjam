d82cf454d3f92f4eb51303d4ba7f0d8a92f1b7f5
niels justesen <nju@it-minds.dk>
2013-09-26 19:15:28 +0200

    Lava bug fixed

--- Assets/Scripts/BlockScripts/Lava.cs
+++ Assets/Scripts/BlockScripts/Lava.cs
@@ -3,12 +3,12 @@ using System.Collections;
 
 public class Lava : MonoBehaviour {
 
-	void OnCollisionEnter (Collision collision) {
+	void OnTriggerEnter (Collider collider) {
 		
 		// If the player touches the lava, squish 'em!
-		if(collision.rigidbody.tag == "Player")
+		if(collider.rigidbody.tag == "Player")
 		{
-			collision.rigidbody.GetComponent<Player>().Squish();
+			collider.rigidbody.GetComponent<Player>().Squish();
 			
 			if(tag=="Mine"){
 				GameObject.Destroy(gameObject);
